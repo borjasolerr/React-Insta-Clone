5dfdcba9226ad1b009e9c7431c97ec6727ce6010
borjasolerr <borjasolrey@gmail.com>
2019-02-18 16:59:43 +0100

    Added coments components and printed them

--- instagram/src/components/PostContainer/PostContainer.js
+++ instagram/src/components/PostContainer/PostContainer.js
@@ -1,5 +1,12 @@
 import React from "react";
+import CommentSection from "../CommentSection/CommentSection";
 
 export default function PostContainer(props) {
-  return <div>PostContainer</div>;
+  const post = props.post;
+  const comments = post.comments;
+  return (
+    <div>
+      <CommentSection comments={comments} />
+    </div>
+  );
 }
