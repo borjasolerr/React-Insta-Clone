785a39a81aef9581d67898fcdeee638629ccccae
borjasolerr <borjasolrey@gmail.com>
2019-02-20 16:31:16 +0100

    Added login component and authenticate.js

--- instagram/src/App.js
+++ instagram/src/App.js
@@ -1,51 +1,28 @@
 import React, { Component } from "react";
-import dummyData from "./dummy-data";
-import PostContainer from "./components/PostContainer/PostContainer";
-import SearchBar from "./components/SearchBar/SearchBar";
+import LoginPage from "./components/LoginPage/LoginPage";
+import Authenticate from "./components/authentication/Authenticate";
+import PostsPage from "./components/PostContainer/PostsPage";
 import "./App.css";
 
-export default class App extends Component {
+class App extends Component {
   constructor(props) {
     super(props);
-    this.state = {
-      dummyData: [],
-      searchValue: ""
-    };
   }
-
-  getData() {
-    this.setState({
-      dummyData: dummyData
-    });
-  }
-
-  componentDidMount() {
-    this.getData();
-  }
-
-  handleSearch = input => {
-    this.setState({
-      searchValue: input
-    });
-  };
 
   render() {
-    return (
-      <div>
-        {/* Search Bar */}
-        <SearchBar searchValue={this.state.searchValue} handleSearch={this.handleSearch} />
-        <div className="environment-container">
-          <div className="app-container">
-            {/* Post Container + filter/search logic */}
-            {this.state.dummyData.map(post => {
-              if (post.username.indexOf(this.state.searchValue) === -1) {
-                return null;
-              }
-              return <PostContainer post={post} timestamp={post.timestamp} key={post.timestamp} comments={post.comments} likes={post.likes} />;
-            })}
-          </div>
-        </div>
-      </div>
-    );
+    return <PostsPage />;
   }
 }
+
+class Login extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return <LoginPage />;
+  }
+}
+
+const AuthenticatedApp = Authenticate(App, Login);
+export default AuthenticatedApp;
---
+++ instagram/src/components/authentication/Authenticate.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+export default function authenticate(Component, LoginComponent) {
+  return class Authenticate extends React.Component {
+    render() {
+      if (true) {
+        return <Component />;
+      }
+      return <LoginComponent />;
+    }
+  };
+}
