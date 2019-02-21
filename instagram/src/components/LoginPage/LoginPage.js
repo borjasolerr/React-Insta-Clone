db3081d5fd0a35a581515dcaf6c5bb04359dc890
borjasolerr <borjasolrey@gmail.com>
2019-02-21 17:06:41 +0100

    Styled components for login page

--- instagram/src/components/LoginPage/LoginPage.js
+++ instagram/src/components/LoginPage/LoginPage.js
@@ -1,4 +1,5 @@
 import React from "react";
+import styled from "styled-components";
 
 export default function LoginPage(props) {
   const login = () => props.login();
@@ -12,13 +13,29 @@ export default function LoginPage(props) {
   };
   const loginName = props.loginName;
   const loginPassword = props.loginPassword;
+  const isEnabled = loginName.length > 0 && loginPassword.length > 0;
   return (
-    <form onSubmit={login}>
-      Name: <input value={loginName} onChange={handleAddNameInput} />
-      Password: <input value={loginPassword} type="password" onChange={handleAddPasswordInput} />{" "}
-      <button type="button" onClick={login}>
-        Submit
-      </button>
-    </form>
+    <StyledLogin>
+      <form onSubmit={login}>
+        Name: <input value={loginName} onChange={handleAddNameInput} />
+        Password: <input value={loginPassword} type="password" onChange={handleAddPasswordInput} />
+        <button type="button" disabled={!isEnabled} onClick={login}>
+          Submit
+        </button>
+      </form>
+    </StyledLogin>
   );
 }
+
+const StyledLogin = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  form {
+    height: 20vh;
+    width: 50vw;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+`;
